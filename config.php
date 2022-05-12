e7f272bf82845cc97002a6a2b253b30977b4f53c
Giuseppe Spataro <spatyx@gmail.com>
2022-05-12 12:51:07 +0200

    update(config.php) add API_ACCESS_KEY

--- config.php
+++ config.php
@@ -8,3 +8,7 @@ define("DB_USERNAME", "username");
 define("DB_PASSWORD", "password");
 define("DB_NAME", "pdash");
 define("DB_PORT", 3306);
+
+// API Configurations
+
+define("API_ACCESS_KEY", "thestrongestpassword");
