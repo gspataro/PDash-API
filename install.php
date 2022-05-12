3b90ea9c18452e57e66c2e0e0bb06e8c60467103
Giuseppe Spataro <spatyx@gmail.com>
2022-05-12 12:54:18 +0200

    new(install.php): add install script
    Add install script to create database tables and make it easier to work on the project from different machines.

---
+++ install.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * PDash API - PHP CRUD API for managing to do and notes
+ * CLI installer
+ *
+ * @author Giuseppe Spataro <https://github.com/gspataro>
+ * @version 1.0.0
+ */
+
+use GSpataro\Database;
+
+if (php_sapi_name() !== 'cli') {
+    die("Access granted only via CLI.");
+}
+
+require_once __DIR__ . "/source/directories.php";
+require_once DIR_ROOT . "/config.php";
+require_once DIR_VENDOR . "/autoload.php";
+
+// Initialize database connection
+
+$database = new Database\Connection(
+    DB_DRIVER,
+    DB_HOSTNAME,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
+    DB_PORT
+);
+
+$queriesQueue = [];
+
+// Create todo table
+
+$queriesQueue[] = $database->prepare("CREATE TABLE IF NOT EXISTS todo (
+    id INT NOT NULL AUTO_INCREMENT,
+    content VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+)");
+
+// Execute queries
+
+foreach ($queriesQueue as $query) {
+    $query->execute();
+}
