909727bcc4aa1686b601f02bad335ef33584f1a6
Giuseppe Spataro <spatyx@gmail.com>
2022-05-12 12:53:46 +0200

    update(routes.php) add new routes

--- source/application/routes.php
+++ source/application/routes.php
@@ -1,13 +1,15 @@
 <?php
 
 use PDash\Controller;
-
+use PDash\Middleware;
 use GSpataro\Routing\Method;
+use PDash\Middleware\AuthMiddleware;
 
 return [
     "home" => [
         "path" => "/",
-        "callback" => [Controller\HomeController::class, "index"]
+        "callback" => [Controller\HomeController::class, "index"],
+        "middlewares" => [Middleware\AuthMiddleware::class]
     ],
     "error404" => [
         "path" => "/pageNotFound",
@@ -16,5 +18,20 @@ return [
     "error405" => [
         "path" => "/methodNotAllowed",
         "callback" => [Controller\HomeController::class, "methodNotAllowed"]
+    ],
+    "accessDenied" => [
+        "path" => "/accessDenied",
+        "callback" => [Controller\HomeController::class, "accessDenied"]
+    ],
+
+    "todo.list" => [
+        "path" => "/todo/list",
+        "callback" => [Controller\TodoController::class, "list"],
+        "middlewares" => [Middleware\AuthMiddleware::class]
+    ],
+    "todo.insert" => [
+        "path" => "/todo/insert",
+        "callback" => [Controller\TodoController::class, "insert"],
+        "methods" => [Method::POST]
     ]
 ];
